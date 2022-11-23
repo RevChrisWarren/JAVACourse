d0024c3851435b3a68ad41c0da9f9624164e5d0e
Chris Warren <chriswarren37129@gmail.com>
2022-11-23 12:09:10 -0600

    conditionals

---
+++ ConditionalsTutorial/src/IfElseStatements.java
@@ -0,0 +1,17 @@
+
+public class IfElseStatements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i = 25;
+if (i == 25) {
+	System.out.println("i is equal to 25");
+}else if (i == 24) {
+	System.out.println("i is equal to 24");
+}
+else {
+	System.out.println("i is not equal to 25");
+}
+	}
+
+}
