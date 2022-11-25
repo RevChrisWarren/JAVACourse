7be66ed0d5ee0b3d999b023bfd935f7c0035aa83
Chris Warren <chriswarren37129@gmail.com>
2022-11-24 19:47:37 -0600

    practicing for loops

---
+++ ConditionalsTutorial/src/MyNumber.java
@@ -0,0 +1,50 @@
+
+public class MyNumber {
+
+	private int num;
+
+	public MyNumber(int num) {
+		this.num = num;
+	}
+		public boolean isPrime(){
+			if (num < 2) {
+				return false;
+			}
+			for (int i =2; i <= num-1; i++) {
+			if (num % i ==0) {
+				return false;
+			}
+			} return true;
+		
+}
+		public int sumUpToN() {
+			int res = 0;
+			for (int i = 1; i <= num; i++) {
+				res = res + i;
+			}
+			return res;
+		}
+		
+		public int sumOfDivisors() {
+			int res = 0;
+			for (int i = 2; i< num; i++) {
+				if(num % i== 0) {
+					res = res+i;
+				}	
+			}
+			return res;
+			
+	
+}
+
+			public void printNumberTriangle() {
+				for (int i = 1; i <= num; i++) {
+					System.out.print(i+ " ");
+					
+						
+	}
+				
+}
+		
+}
+
---
+++ ConditionalsTutorial/src/MyNumberRunner.java
@@ -0,0 +1,18 @@
+
+public class MyNumberRunner {
+
+	public static void main(String[] args) {
+		MyNumber number = new MyNumber(5);
+boolean isPrime = number.isPrime();
+System.out.println("isPrime " + isPrime);
+int sum = number.sumUpToN();
+System.out.println("sum up to n " + sum);
+
+int sumofd = number.sumOfDivisors();
+System.out.println("Sum of Divisors = " +sumofd);
+
+number.printNumberTriangle();
+		
+	}
+
+}
