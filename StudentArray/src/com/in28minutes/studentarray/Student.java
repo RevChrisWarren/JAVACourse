0c2156672adbcd29840662e2b1287f41132393e8
Chris Warren <chriswarren37129@gmail.com>
2022-11-28 10:14:17 -0600

    maximum minimum average

--- StudentArray/src/com/in28minutes/studentarray/Student.java
+++ StudentArray/src/com/in28minutes/studentarray/Student.java
@@ -1,5 +1,50 @@
 package com.in28minutes.studentarray;
 
-public class Student {
+import java.math.BigDecimal;
 
+public class Student {
+	
+	private String name;
+	private int[] marks;
+
+	public Student(String name, int[] marks) {
+	this.name = name;
+	this.marks = marks;
+	}
+	public int getNumberOfMarks() {
+	return marks.length;
+	}
+	public int getTotalSumOfMarks() {
+		int sum = 0;
+		for (int i=0; i<marks.length; i++) {
+			sum= sum+marks[i];
+		}
+		return sum;
+	}
+	public int getMaximumMark() {
+		int maximum = Integer.MIN_VALUE;
+		for (int mark:marks) {
+			if (mark > maximum) {
+				maximum = mark;
+			}
+			
+		}
+		return maximum;
+	}
+	public int getMinimumMark() {
+		int minimum = Integer.MAX_VALUE;
+		for (int mark : marks) {
+			if (mark < minimum) {
+				minimum = mark;
+			}
+					}
+		return minimum;
+	
+}
+//	public BigDecimal average = getAverageMarks() {
+//		BigDecimal sum = 0;
+//		for (int i=0; i<marks.length; i++) {
+//			sum= sum + marks[i];
+//		}return sum / marks.length;
+//	}
 }
--- StudentArray/src/com/in28minutes/studentarray/StudentMarksArrayRunner.java
+++ StudentArray/src/com/in28minutes/studentarray/StudentMarksArrayRunner.java
@@ -3,7 +3,17 @@ package com.in28minutes.studentarray;
 public class StudentMarksArrayRunner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] marks = {98, 99, 94, 90};
+	Student student = new Student("name", marks);
+	int number = student.getNumberOfMarks();
+	System.out.println("number of marks: " + number);
+	int sum = student.getTotalSumOfMarks();
+	System.out.println("Sum of Marks: " + sum);
+	int maximumMark = student.getMaximumMark();
+	System.out.println("Maximum mark: " + maximumMark);
+	int minimumMark = student.getMinimumMark();
+	System.out.println("Minimum Mark: " + minimumMark);
+//	BigDecimal average = student.getAverageMarks();
 
 	}
 
