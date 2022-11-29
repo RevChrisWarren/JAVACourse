83c851f7dacdebfbe96e226a751ca55c137f8c13
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-11-29 14:43:48 -0600

    Working with Arrays2

--- StudentArray/src/com/in28minutes/studentarray/Student.java
+++ StudentArray/src/com/in28minutes/studentarray/Student.java
@@ -1,50 +1,56 @@
 package com.in28minutes.studentarray;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Student {
-	
+
 	private String name;
 	private int[] marks;
 
 	public Student(String name, int[] marks) {
-	this.name = name;
-	this.marks = marks;
+		this.name = name;
+		this.marks = marks;
 	}
+
 	public int getNumberOfMarks() {
-	return marks.length;
+		return marks.length;
 	}
+
 	public int getTotalSumOfMarks() {
 		int sum = 0;
-		for (int i=0; i<marks.length; i++) {
-			sum= sum+marks[i];
+		for (int i = 0; i < marks.length; i++) {
+			sum = sum + marks[i];
 		}
 		return sum;
 	}
+
 	public int getMaximumMark() {
 		int maximum = Integer.MIN_VALUE;
-		for (int mark:marks) {
+		for (int mark : marks) {
 			if (mark > maximum) {
 				maximum = mark;
 			}
-			
+
 		}
 		return maximum;
 	}
+
 	public int getMinimumMark() {
 		int minimum = Integer.MAX_VALUE;
 		for (int mark : marks) {
 			if (mark < minimum) {
 				minimum = mark;
 			}
-					}
+		}
 		return minimum;
-	
-}
-//	public BigDecimal average = getAverageMarks() {
-//		BigDecimal sum = 0;
-//		for (int i=0; i<marks.length; i++) {
-//			sum= sum + marks[i];
-//		}return sum / marks.length;
-//	}
+
+	}
+
+	public BigDecimal getAverageMarks() {
+		int sum = getTotalSumOfMarks();
+		int number = getNumberOfMarks();
+
+		return new BigDecimal(sum).divide(new BigDecimal(number), 3, RoundingMode.UP);
+	}
 }
--- StudentArray/src/com/in28minutes/studentarray/StudentMarksArrayRunner.java
+++ StudentArray/src/com/in28minutes/studentarray/StudentMarksArrayRunner.java
@@ -1,20 +1,22 @@
 package com.in28minutes.studentarray;
 
+import java.math.BigDecimal;
+
 public class StudentMarksArrayRunner {
 
 	public static void main(String[] args) {
-		int[] marks = {98, 99, 94, 90};
-	Student student = new Student("name", marks);
-	int number = student.getNumberOfMarks();
-	System.out.println("number of marks: " + number);
-	int sum = student.getTotalSumOfMarks();
-	System.out.println("Sum of Marks: " + sum);
-	int maximumMark = student.getMaximumMark();
-	System.out.println("Maximum mark: " + maximumMark);
-	int minimumMark = student.getMinimumMark();
-	System.out.println("Minimum Mark: " + minimumMark);
-//	BigDecimal average = student.getAverageMarks();
-
+		int[] marks = { 98, 99, 94, 90 };
+		Student student = new Student("name", marks);
+		int number = student.getNumberOfMarks();
+		System.out.println("number of marks: " + number);
+		int sum = student.getTotalSumOfMarks();
+		System.out.println("Sum of Marks: " + sum);
+		int maximumMark = student.getMaximumMark();
+		System.out.println("Maximum mark: " + maximumMark);
+		int minimumMark = student.getMinimumMark();
+		System.out.println("Minimum Mark: " + minimumMark);
+		BigDecimal average = student.getAverageMarks();
+		System.out.println("Average: " + average);
 	}
 
 }
