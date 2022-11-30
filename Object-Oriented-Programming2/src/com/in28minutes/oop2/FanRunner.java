4d545d1147ce54ce830a03e36de3b14cadba422b
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-11-29 18:57:34 -0600

    OOP2

---
+++ Object-Oriented-Programming2/src/com/in28minutes/oop2/FanRunner.java
@@ -0,0 +1,11 @@
+package com.in28minutes.oop2;
+
+public class FanRunner {
+
+	public static void main(String[] args) {
+		Fan fan = new Fan("Manufacturer1", 0.45, "Gray");
+		System.out.println(fan);
+
+	}
+
+}
