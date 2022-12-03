9dcf099de45a72590ac9dee06694de40131b8aa0
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-12-03 15:47:23 -0600

    generics

---
+++ collections/src/collections/GenericsRunner.java
@@ -0,0 +1,11 @@
+package collections;
+
+public class GenericsRunner {
+
+	public static void main(String[] args) {
+		MyCustomList list = new MyCustomList();
+		list.addElement("Element1");
+
+	}
+
+}
