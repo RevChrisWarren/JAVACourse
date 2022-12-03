0b42d93ace0f74fc58b71362220f2ef92cb04843
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-12-03 16:01:24 -0600

    using type T as generic type

--- collections/src/collections/GenericsRunner.java
+++ collections/src/collections/GenericsRunner.java
@@ -3,8 +3,23 @@ package collections;
 public class GenericsRunner {
 
 	public static void main(String[] args) {
-		MyCustomList list = new MyCustomList();
+		MyCustomList<String> list = new MyCustomList<>();
 		list.addElement("Element1");
+		list.addElement("Element2");
+
+		String value = list.get(0);
+
+		System.out.println(list);
+		System.out.println(value);
+
+		MyCustomList<Integer> list2 = new MyCustomList<>();
+		list2.addElement(Integer.valueOf(5));
+		list2.addElement(Integer.valueOf(7));
+
+		Integer number = list2.get(0);
+
+		System.out.println(list2);
+		System.out.println(number);
 
 	}
 
--- collections/src/collections/MyCustomList.java
+++ collections/src/collections/MyCustomList.java
@@ -2,15 +2,24 @@ package collections;
 
 import java.util.ArrayList;
 
-public class MyCustomList {
-	ArrayList<String> list = new ArrayList<>();
+public class MyCustomList<T> {
+	ArrayList<T> list = new ArrayList<>();
 
-	public void addElement(String element) {
+	public void addElement(T element) {
 		list.add(element);
 
 	}
 
-	public void removeElement(String element) {
+	public void removeElement(T element) {
 		list.remove(element);
 	}
+
+	public String toString() {
+		return list.toString();
+	}
+
+	public T get(int index) {
+		// TODO Auto-generated method stub
+		return list.get(index);
+	};
 }
