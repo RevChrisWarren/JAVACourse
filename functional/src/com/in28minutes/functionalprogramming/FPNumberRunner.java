7a0955d3c2f3eccf718e48040a40be0284c0f92d
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-12-05 17:35:33 -0600

    Stream imtermediate and terminal operations

--- functional/src/com/in28minutes/functionalprogramming/FPNumberRunner.java
+++ functional/src/com/in28minutes/functionalprogramming/FPNumberRunner.java
@@ -8,6 +8,7 @@ public class FPNumberRunner {
 		List<Integer> numbers = List.of(4, 6, 8, 13, 3, 15);
 		numbers.stream().forEach(element -> System.out.println(element));
 
+		// Sum only odd numbers
 		int sum = numbers.stream().filter(element -> element % 2 == 1).reduce(0,
 				(number1, number2) -> number1 + number2);
 
