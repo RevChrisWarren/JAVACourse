eca7f501988612f429a42dbb2ebd64583267008e
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-12-09 21:22:09 -0600

    latestsave

---
+++ tipsnadtricks/src/com/tipsandtricks/BlocksRunner.java
@@ -0,0 +1,12 @@
+package com.tipsandtricks;
+
+public class BlocksRunner {
+//Use blocks 
+	public static void main(String[] args) {
+		if (3 > 2) {
+			System.out.print("3>2");
+		}
+
+	}
+
+}
--- tipsnadtricks/src/com/tipsandtricks/ImportsRunner.java
+++ tipsnadtricks/src/com/tipsandtricks/ImportsRunner.java
@@ -3,8 +3,7 @@ package com.tipsandtricks;
 public class ImportsRunner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String str = "";
 	}
 
 }
