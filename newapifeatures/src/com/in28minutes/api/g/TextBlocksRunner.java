e29a666bf94a36fc6cf75bcc4875e5b21ce5a7cc
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-12-11 19:17:53 -0600

    Text Blocks

---
+++ newapifeatures/src/com/in28minutes/api/g/TextBlocksRunner.java
@@ -0,0 +1,16 @@
+package com.in28minutes.api.g;
+
+public class TextBlocksRunner {
+
+	public static void main(String[] args) {
+		String str = """
+				Line 1
+				Line 2
+				Line 3
+				""";
+
+		System.out.print(str);
+
+	}
+
+}
