06f810b8603fa7201e4bde0a79d016e5c99abf16
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-12-13 10:31:49 -0600

    SpringBoot 2

--- learn-springboot/src/main/java/com/in28minutes/learnspringboot/courses/controller/CourseController.java
+++ learn-springboot/src/main/java/com/in28minutes/learnspringboot/courses/controller/CourseController.java
@@ -14,6 +14,12 @@ public class CourseController {
 	// http://localhost:8080/courses
 	@GetMapping("/courses")
 	public List<Course> getAllCourses() {
-		return Arrays.asList(new Course(1, "Learn Microservices", "in28Minutes"));
+		return Arrays.asList(new Course(1, "Learn Microservices", "in28Minutes"),
+				new Course(2, "Learn Full Stack with Angular and React", "in28minutes"));
+	}
+
+	@GetMapping("courses/1")
+	public Object getOneCourse() {
+		return (new Course(3, "REACT", "Warren"));
 	}
 }
