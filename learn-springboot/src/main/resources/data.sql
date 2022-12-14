7cab6b8cac26b986c1656842b2fbdbfc3607878c
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-12-14 15:50:12 -0600

    learning to use database with JAVA

---
+++ learn-springboot/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into COURSE (ID, AUTHOR, NAME)
+values (100001, 'in28minutes', 'Learn Microservices');
+insert into COURSE (ID, AUTHOR, NAME)
+values (100002, 'in28minutes', 'Learn Full-Stack Development');
+insert into COURSE (ID, AUTHOR, NAME)
+values (100003, 'in28minutes', 'Learn Angular');
+insert into COURSE (ID, AUTHOR, NAME)
+values (100004, 'in28minutes', 'Learn React');
