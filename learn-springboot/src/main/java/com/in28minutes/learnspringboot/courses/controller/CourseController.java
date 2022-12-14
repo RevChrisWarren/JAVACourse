0116478a4313fc0f728ace2ada92a161861ccaab
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-12-14 16:05:24 -0600

    learning to use database with JAVA 2

--- learn-springboot/src/main/java/com/in28minutes/learnspringboot/courses/controller/CourseController.java
+++ learn-springboot/src/main/java/com/in28minutes/learnspringboot/courses/controller/CourseController.java
@@ -1,21 +1,25 @@
 package com.in28minutes.learnspringboot.courses.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.in28minutes.learnspringboot.courses.bean.Course;
+import com.in28minutes.learnspringboot.courses.repository.CourseRepository;
 
 @RestController
 public class CourseController {
+	@Autowired
+	private CourseRepository repository;
 
 	// http://localhost:8080/courses
 	@GetMapping("/courses")
 	public List<Course> getAllCourses() {
-		return Arrays.asList(new Course(1, "Learn Microservices", "in28Minutes"),
-				new Course(2, "Learn Full Stack with Angular and React", "in28minutes"));
+		return repository.findAll();
+//		return Arrays.asList(new Course(1, "Learn Microservices", "in28Minutes"),
+//				new Course(2, "Learn Full Stack with Angular and React", "in28minutes"));
 	}
 
 	@GetMapping("courses/1")
