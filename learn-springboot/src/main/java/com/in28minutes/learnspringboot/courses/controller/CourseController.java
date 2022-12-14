b5e62d8acfc72f5d5108d3d827a55c50ff20168f
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-12-14 16:47:37 -0600

    learning to use database with JAVA 3

--- learn-springboot/src/main/java/com/in28minutes/learnspringboot/courses/controller/CourseController.java
+++ learn-springboot/src/main/java/com/in28minutes/learnspringboot/courses/controller/CourseController.java
@@ -1,9 +1,15 @@
 package com.in28minutes.learnspringboot.courses.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.in28minutes.learnspringboot.courses.bean.Course;
@@ -22,8 +28,28 @@ public class CourseController {
 //				new Course(2, "Learn Full Stack with Angular and React", "in28minutes"));
 	}
 
-	@GetMapping("courses/1")
-	public Object getOneCourse() {
-		return (new Course(3, "REACT", "Warren"));
+	@GetMapping("courses/{id}")
+	public Course getCourseDetails(@PathVariable long id) {
+		Optional<Course> course = repository.findById(id);
+		if (course.isEmpty()) {
+			throw new RuntimeException("Course with that id " + id + " not found");
+		}
+		return course.get();
+		// return (new Course(3, "REACT", "Warren"));
+	}
+
+	@PostMapping("/courses")
+	public void createCourse(@RequestBody Course course) {
+		repository.save(course);
+	}
+
+	@PutMapping("/courses/{id}")
+	public void updateCourse(@PathVariable long id, @RequestBody Course course) {
+		repository.save(course);
+	}
+
+	@DeleteMapping("/courses/{id}")
+	public void deleteCourse(@PathVariable long id) {
+		repository.deleteById(id);
 	}
 }
